8f486a1999d5d433a0013837ef01d8c7e5e83956
meowololo <mark33@inbox.ru>
2020-02-25 02:04:57 +0300

    14 homework done

--- Strings.cpp
+++ Strings.cpp
@@ -8,11 +8,9 @@ int main()
 	std::string something;
 	std::getline(std::cin, something);
 
-	int lastsymbol = something.length() - 1;
-
 	std::cout << "\n" << "Your string: " << something << "\n";
 	std::cout << "Length of string: " << something.length() << "\n";
 	std::cout << "First symbol: " << something[0] << "\n";
-	std::cout << "Last symbol: " << something[lastsymbol] << "\n";
+	std::cout << "Last symbol: " << something[something.length() - 1] << "\n";
 
 }
